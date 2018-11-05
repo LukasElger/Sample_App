efd6d85babb7bfe81f72f9bf3434eed0a1223e66
Lukas Elger <lukaselger01@gmail.com>
2018-11-05 15:52:22 +0100

    Changed user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,20 +73,22 @@ class User < ApplicationRecord
   end
 
   # Returns true if a password reset has expired.
-def password_reset_expired?
-  reset_sent_at < 2.hours.ago
-end
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
-# Returns a user's status feed.
-def feed
-  Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
-                  following_ids: following_ids, user_id: id)
-end
+  # Returns a user's status feed.
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
 
-# Follows a user.
-def follow(other_user)
-  following << other_user
-end
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
 
   # Unfollows a user.
   def unfollow(other_user)
